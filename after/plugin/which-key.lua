842828d5d5fab1d7eabd5647758b56ef8c2bf8c6
Farid Said Tahirshah <farid.said.tahirshah@epiroc.com>
2023-07-10 22:50:51 +0200

    remove useless code

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -220,8 +220,8 @@ keymap("v", "E", ":m '>+1<CR>gv=gv", { silent = true, nowait = true, noremap = t
 keymap("v", "N", ":m '<-2<CR>gv=gv", { silent = true, nowait = true, noremap = true })
 keymap("n", "J", "mzJ`z")
 
-keymap("n","<PageUp>", "<C-u>zz")
-keymap("n","<PageDown>", "<C-d>zz")
+keymap("n", "<PageUp>", "<C-u>zz")
+keymap("n", "<PageDown>", "<C-d>zz")
 
 -- keymap("n", "<C-u>", "<C-u>zz")
 keymap("n", "n", "nzzzv")
@@ -475,61 +475,40 @@ require("gitsigns").setup({
 	end,
 })
 
-local function get_file_type()
-	local file_ext = vim.fn.expand("%:e")
-	if file_ext == "js" or file_ext == "jsx" or file_ext == "ts" or file_ext == "tsx" then
-		return "javascript"
-	elseif file_ext == "cpp" or file_ext == "h" then
-		return "cpp"
-	end
-	return "unknown"
-end
 keymap({ "n", "i" }, "<C-l>", function()
-	local file_name = vim.fn.expand("%:t")
-	local word = vim.fn.expand("<cword>")
-	local file_type = get_file_type()
-
-	local indent = vim.fn.indent(".")
-	local indent_spaces = string.rep(" ", indent)
-    require("logsitter").log()
-
-	-- if file_type == "javascript" then
-		-- local log_statement = string.format('%sconsole.log("%s, %s: ", %s);', indent_spaces, file_name, word, word)
-		-- vim.api.nvim_put({ log_statement }, "l", true, true)
-	-- elseif file_type == "cpp" then
-		-- local log_statement = string.format('%sRFC_printf("%s %s: %%d", %s);', indent_spaces, file_name, word, word)
-		-- if vim.fn.search("std::string\\s*" .. word, "nw") ~= 0 then
-			-- log_statement =
-				-- string.format('%sRFC_printf("%s %s: %%s", %s.c_str());', indent_spaces, file_name, word, word)
-		-- elseif vim.fn.search("\\s*float\\s*" .. word .. "\\|\\s*double\\s*" .. word, "nw") ~= 0 then
-			-- log_statement = string.format('%sRFC_printf("%s %s: %%f", %s);', indent_spaces, file_name, word, word)
-		-- end
-		-- vim.api.nvim_put({ log_statement }, "l", true, true)
-	-- end
+	require("logsitter").log()
 end, { silent = true, noremap = true })
 
-
-function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
-
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-
-
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float", float_opts = { border = "double" } })
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit =
+	Terminal:new({ cmd = "lazygit", hidden = true, direction = "float", float_opts = { border = "double" } })
+local term = Terminal:new({
+	hidden = true,
+	direction = "float",
+	float_opts = { border = "double" },
+	on_open = function(term)
+		vim.cmd("startinsert!")
+	end,
+})
 
 function _Lazygit_toggle()
-  lazygit:toggle()
+	lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>gt", "<cmd>lua _Lazygit_toggle()<CR>", {noremap = true, silent = true})
+keymap("n", "<leader>gt", function()
+	_Lazygit_toggle()
+end, { noremap = true, silent = true })
+
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<C-w>", function()
+		term:close()
+		lazygit:close()
+	end, opts)
+end
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+keymap("n", "<leader>tt", function()
+	term:toggle()
+end, { noremap = true, silent = true })
